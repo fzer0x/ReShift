703a79a6a9daa36832cd7065e5c672372397c484
fzer0x <saschapicasso@gmail.com>
2026-09-18 08:07:32 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "ox.fzer0x.snakeloader"
         minSdk = 29
         targetSdk = 35
-        versionCode = 101
-        versionName = "1.0.1"
+        versionCode = 102
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
